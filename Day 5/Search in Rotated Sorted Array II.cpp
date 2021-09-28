7bd6ba87b0fa9b6485a1ef542aa7c9aeb4992506
ishitagupta2702 <79853573+ishitagupta2702@users.noreply.github.com>
2021-09-28 19:54:12 +0530

    Search in Rotated Sorted Array II

---
+++ Day 5/Search in Rotated Sorted Array II.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include<vector>
+using namespace std;
+
+class Solution {
+public:
+    bool search(vector<int>& nums, int target) {
+        
+        for(int i = 0; i < nums.size(); i++){
+            if(nums[i] == target){
+                return true;
+            }
+        }
+        
+        return false;
+    }
+};
