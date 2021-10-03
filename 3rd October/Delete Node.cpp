8b179a851b2553fa202a6a7212c0ec92091178fc
ishitagupta2702 <79853573+ishitagupta2702@users.noreply.github.com>
2021-10-03 19:27:25 +0530

    Delete a Node in Linked List

---
+++ 3rd October/Delete Node.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+class Solution {
+public:
+    void deleteNode(ListNode* node) {
+        
+        ListNode *temp = node -> next;
+        node -> val = temp -> val;
+        node -> next = temp -> next;
+        
+        
+    }
+};
